75925b446baa7f20225c667c4d934e50c5980046
lipracer <lipracer@gmail.com>
2021-12-02 01:00:07 +0800

    add interview test

---
+++ test/interview.cc
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <bitset>
+
+#include "solution.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+using namespace Solution;
+using namespace type;
+
+TEST(QuitSort, sort) {
+    
+}
+
+template <typename T>
+struct TreeNode {
+    TreeNode *left;
+    TreeNode *right;
+    T data;
+};
+
+TEST(commonAncestors, simple) {
+
+}
